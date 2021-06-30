26e929980576648d6483825703e25da65c489a74
alsrbga <alican.sariboga.acs@gmail.com>
2021-06-30 15:36:57 +0100

    some fixed

---
+++ src/home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Home.css";
+import Banner from "../banner/Banner.js";
+
+
+function Home () {
+    return  (
+        <div className="home">
+        <Banner />
+        </div>
+    )
+} 
+
+
+
+export default Home;
