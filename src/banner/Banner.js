4b62e30efe24b331633bb2f4e35ea78fc2a52acc
alsrbga <alican.sariboga.acs@gmail.com>
2021-07-02 16:56:26 +0100

    part of about added

--- src/banner/Banner.js
+++ src/banner/Banner.js
@@ -22,7 +22,7 @@ const Banner = (props) => {
 
   const next = () => {
     if (animating) return;
-    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    const nextIndex  = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
     setActiveIndex(nextIndex);
   };
 
--- src/home/Home.js
+++ src/home/Home.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import "./Home.css";
 import Banner from "../banner/Banner.js";
-
+import About from "../about/About.js";
 
 function Home () {
     return  (
         <div className="home">
         <Banner />
+        <About />
         </div>
     )
 } 
-
-
 
 export default Home;
